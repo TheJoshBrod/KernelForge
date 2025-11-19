14124379adf1d81f7892b5b0b3bdff2e0b565bf7
joshbrod <joshbrod@umich.edu>
2025-11-19 00:51:14 -0500

    Resnet profiler

--- benchmarks/generate_benchmarks/benchmark_generator.py
+++ benchmarks/generate_benchmarks/benchmark_generator.py
@@ -2,10 +2,14 @@ import torch
 import torch.autograd.profiler as profiler
 import torch.nn.functional as F
 from functools import wraps
+from PIL import Image
 import inspect
 
+
 # from benchmarks.generate_benchmarks.trivial import SimpleModel
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoImageProcessor, AutoModelForImageClassification, AutoConfig
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -93,27 +97,46 @@ for name in dir(F):
 #   Initialize PyTorch Model
 # ****************************
 
-model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-inputs = tokenizer("Hello, this is a test of your tracking code!", return_tensors="pt")
+model_name = "microsoft/resnet-50"
+processor = AutoImageProcessor.from_pretrained(model_name)
+model = AutoModelForImageClassification.from_pretrained(model_name)
 
+image = Image.open("benchmarks/generate_benchmarks/pics/image.png")
+inputs = processor(images=image, return_tensors="pt")
+
+config = AutoConfig.from_pretrained(model_name)
+id2label = config.id2label
 
 # ****************************
 #        ATen Profiler 
 # ****************************
 
 device_str = 'cuda' if torch.cuda.is_available() else 'cpu'
+model.to(device_str)
+inputs = {k: v.to(device_str) for k, v in inputs.items()}
 with profiler.profile(record_shapes=True, use_device=device_str) as prof:
     with profiler.record_function("forward"):
         with torch.no_grad():
             outputs = model(**inputs)
+            probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+            top5 = probs.topk(5)
+            print("\nRAW TOP-5 TENSOR:\n", top5)
+
+            print("\nDECODED PREDICTIONS:")
+            for idx in top5.indices.squeeze().tolist():
+                print(f"{idx}: {id2label[idx]} ({float(probs[0][idx]):.5f})")
+
 print(prof.key_averages().table(sort_by="count", row_limit=50))
 
 # ****************************
 #  Export PyTorch API In/Out 
 # ****************************
 
+
 print("Saving....")
+print(model.__class__.__name__)
 torch.save(calls, f"benchmarks/generate_benchmarks/{model.__class__.__name__}.pt")
 print("Saved to output.pt")
+
+for func, iters in calls.items():
+    print(f"{func}: {len(iters)}")
