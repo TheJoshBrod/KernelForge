eb022f27f47c1b33dc987feddf1a4508fd67e45f
joshbrod <joshbrod@umich.edu>
2025-12-15 18:32:24 -0500

    removed extra folders

--- benchmarks/run_benchmarks/compiled_kernels/torch_nn_functional_gelu/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <torch/extension.h>
-torch::Tensor launch(torch::Tensor input);
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-m.def("launch", torch::wrap_pybind_function(launch), "launch");
-}
--- benchmarks/run_benchmarks/compiled_kernels/torch_nn_functional_softmax/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <torch/extension.h>
-torch::Tensor launch(torch::Tensor input, int64_t dim, int64_t _stacklevel, c10::optional<c10::ScalarType> dtype);
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-m.def("launch", torch::wrap_pybind_function(launch), "launch");
-}
